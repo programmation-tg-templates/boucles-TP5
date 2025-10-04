4de1aff8096a58c2b02f19e4c60bbf89e57b4419
François Roland <francois.roland@heh.be>
2025-10-04 17:28:13 +0200

    Write instructions and unit test.

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
-# base
+# Boucles - TP5 - La factorielle
 
-Consignes
+## Consignes
+
+Créer une fonction `factorielle(n: number): number` qui renvoie la factorielle d’un nombre entré en paramètre.
+
+## Rappel mathématique
+
+La factorielle d'un nombre entier naturel `n` est le produit des nombres entiers strictement positifs inférieurs ou égaux à `n`.
+
+Par convention, la factorielle de 0 est 1. 
+
+## Exemples
+
+- `factorielle(1)` => $1! = 1$
+- `factorielle(2)` => $2! = 2 \times 1 = 2$
+- `factorielle(3)` => $3! = 3 \times 2 \times 1 = 6$
+- `factorielle(4)` => $4! = 4 \times 3 \times 2 \times 1 = 6$
+- `factorielle(5)` => $5! = 5 \times 4 \times 3 \times 2 \times 1 = 120$
--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -1,9 +1,29 @@
 // Tests unitaires écrits par l'enseignant. Ne modifiez pas ce fichier.
 
-import {...} from '../index';
+import {factorielle} from '../index';
 
-describe('...', () => {
-    test('...', () => {
-        expect(false).toBe(true);
+describe('La factorielle de ', () => {
+    test('1 vaut 1', () => {
+        expect(factorielle(1)).toEqual(1);
+    });
+
+    test('2 vaut 2', () => {
+        expect(factorielle(2)).toEqual(2);
+    });
+
+    test('3 vaut 6', () => {
+        expect(factorielle(3)).toEqual(6);
+    });
+
+    test('4 vaut 24', () => {
+        expect(factorielle(4)).toEqual(24);
+    });
+
+    test('5 vaut 120', () => {
+        expect(factorielle(5)).toEqual(120);
+    });
+
+    test('0 vaut 1', () => {
+        expect(factorielle(0)).toEqual(1);
     });
 });
